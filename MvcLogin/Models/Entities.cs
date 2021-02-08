8bcc20e080dbc4d1b471c9d774c009682c144d8d
sahar1989 <sahar.hosseini@hotmail.com>
2021-02-08 13:45:36 +0000

    first commit

---
+++ MvcLogin/Models/Entities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+namespace MvcLogin.Models
+{
+    public class Entities : DbContext
+    {
+        public Entities() : base("MvcLoginDB") { }
+        public DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Database.SetInitializer<demoEntities>(null);
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ MvcLoginTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcLogin.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Threading.Tasks;
+using MvcLogin.Controllers.Tests;
+
+namespace MvcLogin.Controllers.Tests
+{
+    [TestClass()]
+    public class HomeControllerTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void RegisterTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void IndexTest1()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void RegisterTest1()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void RegisterTest2()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void LoginTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void LoginTest1()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void LogoutTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void GetMD5Test()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
